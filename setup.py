2e18e5f0b19bfcd26574250cf6dd391105a65141
danielpdwalker <danielpdwalker@gmail.com>
2021-10-25 11:49:35 +0100

    Updated setup.py to handle both yaml formats

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version="0.1.0",
     description="Meltano project file bundle of Matatika datasets for Spotify",
     packages=find_packages(),
-    package_data={"bundle": ["analyze/datasets/tap-spotify/*.yml"]},
+    package_data={"bundle": [
+        "analyze/datasets/tap-spotify/*.yml",
+        "analyze/datasets/tap-spotify/*.yaml"        
+        ]}
 )
