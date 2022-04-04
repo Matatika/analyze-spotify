8ba9f80ed9a3326c3560ac92341cab3470efdf55
danielpdwalker <danielpdwalker@gmail.com>
2022-04-04 13:30:29 +0100

    Bumped version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-spotify",
-    version="0.1.0",
+    version="0.4.0",
     description="Meltano project file bundle of Matatika datasets for Spotify",
     packages=find_packages(),
     package_data={"bundle": [
